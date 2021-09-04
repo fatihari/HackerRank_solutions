bed6a8ac9b1bb8f1582ae5889e0e45a6ef0f20d5
fatihari <57245919+fatihari@users.noreply.github.com>
2021-09-04 21:04:55 +0300

    First Commit

---
+++ SQL/BasicSelect/10-Weather-Observation-Station-5.sql
@@ -0,0 +1,15 @@
+ /**
+ *  @author Fatih ARI - 04.09.2021
+ */
+ 
+(
+SELECT city, LENGTH(city) FROM station
+ORDER BY LENGTH(city) ASC, city ASC
+LIMIT 1
+)
+UNION
+(
+SELECT city, LENGTH(city) FROM station
+ORDER BY LENGTH(city) DESC, city ASC
+LIMIT 1
+);
